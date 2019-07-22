8bb4c77ffa385849fd681ea658b2afac4c92be92
yankees1fan2002 <yankees1fan2002@gmail.com>
2019-07-22 17:01:38 -0400

    Updated gradle file settings and created an empty teleop program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Sarthak on 7/22/2019.
+ */
+public class Teleop extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
